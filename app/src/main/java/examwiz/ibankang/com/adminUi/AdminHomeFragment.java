494fb919be4c7a11069071212344d9cfcd4f1fcc
ibankang <mribankang@gmail.com>
2024-03-30 20:54:47 +0530

    get user schedule data along with exam details

--- app/src/main/java/examwiz/ibankang/com/adminUi/AdminHomeFragment.java
+++ app/src/main/java/examwiz/ibankang/com/adminUi/AdminHomeFragment.java
@@ -1,10 +1,15 @@
 package examwiz.ibankang.com.adminUi;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,55 +17,195 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import examwiz.ibankang.com.R;
+import examwiz.ibankang.com.SubAdmin.subadmin_exam_details_adapter;
+import examwiz.ibankang.com.SubAdmin.subadmin_exam_schedule_details_adapter;
 import examwiz.ibankang.com.databinding.FragmentAdminHomeBinding;
+import examwiz.ibankang.com.view_model;
 
 public class AdminHomeFragment extends Fragment {
 
-    FragmentAdminHomeBinding binding;
+    String account_type = "user";
     RecyclerView recyclerView;
+    SharedPreferences sharedpreferences;
+    FirebaseUser firebaseUser;
+    private FirebaseFirestore db;
+    TextView textView;
+    String guid = null, exam_uid = null;
+
+    View view;
+    String rollno = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        // Get user ID
+        guid = firebaseUser.getUid();
 
-        binding = FragmentAdminHomeBinding.inflate(inflater, container, false);
+        db = FirebaseFirestore.getInstance();
 
-        recyclerView = binding.rvExam;
+        view = inflater.inflate(R.layout.fragment_admin_home, container, false);
 
-        List<ExamDetails> examDetailsList = new ArrayList<>();
-        examDetailsList.add(new ExamDetails("Mathematics", "WAEC", "2021", "12th May", "10:00am"));
-        examDetailsList.add(new ExamDetails("Physics", "WAEC", "2021", "12th May", "10:00am"));
-        examDetailsList.add(new ExamDetails("Chemistry", "WAEC", "2021", "12th May", "10:00am"));
-        examDetailsList.add(new ExamDetails("Biology", "WAEC", "2021", "12th May", "10:00am"));
-        examDetailsList.add(new ExamDetails("English", "WAEC", "2021", "12th May", "10:00am"));
-        examDetailsList.add(new ExamDetails("Agric", "WAEC", "2021", "12th May", "10:00am"));
-        examDetailsList.add(new ExamDetails("Economics", "WAEC", "2021", "12th May", "10:00am"));
-        examDetailsList.add(new ExamDetails("Government", "WAEC", "2021", "12th May", "10:00am"));
-        examDetailsList.add(new ExamDetails("Literature", "WAEC", "2021", "12th May", "10:00am"));
-        examDetailsList.add(new ExamDetails("CRS", "WAEC", "2021", "12th May", "10:00am"));
+        recyclerView = view.findViewById(R.id.recycler_view);
+        // Retrieve year_uid from arguments
+        if (getArguments() != null) {
+            rollno = getArguments().getString("rollno");
+        }
 
-        AdminAdapter adminAdapter = new AdminAdapter(getContext(), examDetailsList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(adminAdapter);
+        get_exam_details();
 
-        ImageSlider imageSlider = binding.imageSlider;
+
+        //slider
+        ImageSlider imageSlider = view.findViewById(R.id.image_slider);
         ArrayList<SlideModel> slideModels = new ArrayList<>();
 
-        slideModels.add(new SlideModel(R.drawable.wallpaper1, ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.wallpaper1, ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.wallpaper1, ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.wallpaper1, ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.wallpaper1, ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.wallpaper1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.slider2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.slider3, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.slider4, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.slider5, ScaleTypes.FIT));
 
         imageSlider.setImageList(slideModels, ScaleTypes.FIT);
 
-        return binding.getRoot();
+        return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        get_user_rollno();
+    }
+
+    void get_exam_details(){
+
+        if (account_type.equals("admin") || account_type.equals("subadmin")) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm a", Locale.getDefault());
+            LinearLayoutManager mlinearLayoutManager = new LinearLayoutManager(getContext());
+            recyclerView.setLayoutManager(mlinearLayoutManager);
+            ArrayList<view_model> dataholder = new ArrayList<>();
+            subadmin_exam_details_adapter wAdapter = new subadmin_exam_details_adapter(dataholder, getContext());
+            CollectionReference collectionReference = db.collection("exam");
+            collectionReference
+                    //.orderBy("date_time",Query.Direction.DESCENDING)
+                    .get().addOnCompleteListener(task -> {
+                        if (task.isSuccessful() && task.getResult().size() > 0) {
+                            for (QueryDocumentSnapshot s : task.getResult()) {
+
+                                view_model obj = new view_model(
+                                        s.getString("exam_title"),//1
+                                        s.getString("exam_category"),//2
+                                        s.getString("exam_date"),//3
+                                        s.getString("exam_start"),//4
+                                        //String.valueOf(dateFormat.format(s.getTimestamp("dob").toDate())),//4
+                                        s.getString("exam_end"),//5
+                                        s.getString("seating_plan_live"),//6
+                                        "",//7
+                                        "",//8
+                                        "",//9
+                                        s.getString("exam_uid"));//10
+
+                                dataholder.add(obj);
+                            }
+                            recyclerView.setAdapter(wAdapter);
+                        } else {
+
+                        }
+                    });
+        }
+
+    }
+
+    private void get_exam_details_user() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm a", Locale.getDefault());
+        LinearLayoutManager mlinearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mlinearLayoutManager);
+        ArrayList<view_model> dataholder = new ArrayList<>();
+        subadmin_exam_schedule_details_adapter wAdapter = new subadmin_exam_schedule_details_adapter(dataholder, getContext());
+        CollectionReference collectionReference = db.collection("schedule");
+        collectionReference
+                .whereEqualTo("rollno",rollno)
+                //.orderBy("date_time",Query.Direction.DESCENDING)
+                .get().addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult().size() > 0) {
+                        for (QueryDocumentSnapshot s : task.getResult()) {
+
+                            view_model obj = new view_model(
+                                    s.getString("roomno"),//1
+//                                    s.getString("exam_category"),//2
+//                                    s.getString("exam_date"),//3
+//                                    s.getString("exam_start"),//4
+//                                    //String.valueOf(dateFormat.format(s.getTimestamp("dob").toDate())),//4
+//                                    s.getString("exam_end"),//5
+//                                    s.getString("seating_plan_live"),//6
+                                    "",//7
+                                    "",//7
+                                    "",//7
+                                    "",//7
+                                    "",//7
+                                    "",//7
+                                    "",//8
+                                    "",//9
+                                    s.getString("exam_uid"));//10
+
+                            dataholder.add(obj);
+                        }
+                        recyclerView.setAdapter(wAdapter);
+                    } else {
+
+                    }
+                });
+
+    }
+
+    private void get_user_rollno() {
+
+
+        // Get Firebase user
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            // Get user ID
+            String userId = firebaseUser.getUid();
+
+            // Get Firestore instance
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            // Get document reference
+            DocumentReference docRef = db.collection("account").document(userId);
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            // Retrieve user data
+                            rollno = document.getString("roll_no");
+                             String name = document.getString("name");
+                            Toast.makeText(getContext(), name+"_"+rollno, Toast.LENGTH_SHORT).show();
+
+                            get_exam_details_user();
+                        }
+                    } else {
+                        Log.d("MainActivity", "Document retrieval failed:", task.getException());
+                    }
+                }
+            });
+        }
+    }
+
 }
